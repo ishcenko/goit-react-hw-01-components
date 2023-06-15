270ef32f7c2e588ed1d6909eb0c87abf011f00c2
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-06-15 12:31:45 +0300

    User Profile create

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
+// components/App.js
+import React from 'react';
+import user from './Profile/user.json';
+import Profile from './Profile/Profile';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>User Profile</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
+
+export default App;

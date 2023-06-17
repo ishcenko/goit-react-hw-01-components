0c8eead37289d84be8f3923ab299ca0b5a72fff6
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-06-17 13:25:35 +0300

    1,2,3,4-task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,18 @@
-// components/App.js
-import React from 'react';
 import user from './Profile/user.json';
 import Profile from './Profile/Profile';
 
-const App = () => {
+import data from './Statistics/data.json';
+import Statistics from './Statistics/Statistics';
+
+import friends from './FriendList/friends.json';
+import FriendList from './FriendList/FriendList';
+
+import transactions from './TransactionHistory/transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+export default function App() {
   return (
     <div>
-      <h1>User Profile</h1>
       <Profile
         username={user.username}
         tag={user.tag}
@@ -14,8 +20,56 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
-};
+}
 
-export default App;
+
+
+
+
+
+// // components/App.js
+// import React from 'react';
+
+// import user from './Profile/user.json';
+// import Profile from './Profile/Profile';
+
+// import friends from './FriendList/friends.json';
+// import FriendList from './FriendList/FriendList';
+
+
+// const Profile = () => {
+//   return (
+//     <div>
+//       <h1>User Profile</h1>
+//       <Profile
+//         username={user.username}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}
+//       />
+//     </div>
+//   );
+// };
+
+// // export default App;
+
+// const FriendListItem = ({ avatar, name, isOnline }) => {
+//   return (
+//     <li className="item">
+//       <span
+//         className="status"
+//         style={{ backgroundColor: friends.isOnline ? 'green' : 'red' }}
+//       ></span>
+//       <img className="avatar" src={friends.avatar} alt="User avatar" width="48" />
+//       <p className="name">{friends.name}</p>
+//     </li>
+//   );
+// };
+
+// // export default FriendList;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,35 +1,95 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  ProfileWrap,
+  Description,
+  Avatar,
+  Name,
+  Tag,
+  Location,
+  Stats,
+  ListItem,
+  Label,
+  Quantity,
+} from './Profile.styled';
 
 const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div className="profile">
-      <div className="description">
-        <img
-          src={avatar}
-          alt="User avatar"
-          className="avatar"
-        />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
-      </div>
+    <ProfileWrap>
+      <Description>
+        <Avatar src={avatar} alt={username} />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
-        </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
-        </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+      <Stats>
+        <ListItem>
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </ListItem>
+        <ListItem>
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </ListItem>
+        <ListItem>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </ListItem>
+      </Stats>
+    </ProfileWrap>
   );
 };
 
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
+
 export default Profile;
+
+
+
+// import React from 'react';
+
+// const Profile = ({ username, tag, location, avatar, stats }) => {
+//   return (
+//     <div className="profile">
+//       <div className="description">
+//         <img
+//           src={avatar}
+//           alt="User avatar"
+//           className="avatar"
+//         />
+//         <p className="name">{username}</p>
+//         <p className="tag">@{tag}</p>
+//         <p className="location">{location}</p>
+//       </div>
+
+//       <ul className="stats">
+//         <li>
+//           <span className="label">Followers</span>
+//           <span className="quantity">{stats.followers}</span>
+//         </li>
+//         <li>
+//           <span className="label">Views</span>
+//           <span className="quantity">{stats.views}</span>
+//         </li>
+//         <li>
+//           <span className="label">Likes</span>
+//           <span className="quantity">{stats.likes}</span>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Profile;

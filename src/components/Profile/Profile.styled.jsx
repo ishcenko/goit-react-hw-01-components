2e6a9f5817f2609cb61a036536a770fab77e8f89
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-06-17 20:51:02 +0300

    style 1 task

--- src/components/Profile/Profile.styled.jsx
+++ src/components/Profile/Profile.styled.jsx
@@ -1,75 +1,77 @@
 import styled from '@emotion/styled';
 
 export const ProfileWrap = styled.div`
-  width: 340px;
+  width: 800px;
   margin: 0 auto;
-  padding: 70px 50px;
-  background-color: darkgrey;
+  padding: 170px 50px;
+  background: linear-gradient(to right, #50c4f2, #34e89e);
+  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.5);
   border-radius: 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 `;
 
 export const Description = styled.div`
-  width: 240px;
-  height: 220px;
+  width: 350px;
+  height: 280px;
   margin: 0 auto;
-  padding: 30px;
-  background-color: #ffffff;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
+  padding: 50px;
+  background: linear-gradient(90deg,#f1e792,#ecd1aa);  
+  border-radius: 25px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 `;
 
 export const Avatar = styled.img`
-  width: 100px;
+  width: 170px;
   display: block;
-  margin: 0 auto 20px;
+  margin: 0 auto 35px;
   border-radius: 50%;
 `;
 
 export const Name = styled.p`
   text-align: center;
   font-weight: 700;
-  font-size: 22px;
-  margin-bottom: 15px;
+  font-size: 24px;
+  margin-bottom: 20px;
 `;
 
 export const Tag = styled.p`
+  font-size: 20px;
   text-align: center;
-  color: darkgrey;
-  margin-bottom: 10px;
+  color: black;
+  margin-bottom: 15px;
 `;
 
 export const Location = styled.p`
+  font-size: 20px;
   text-align: center;
-  color: darkgrey;
+  color: black;
 `;
 
 export const Stats = styled.ul`
-  width: 302px;
+  width: 450px;
   height: 100px;
-  background-color: gainsboro;
+  background: linear-gradient(90deg, #9ad7ee, #f2f928);
   margin: 0 auto;
+
+  margin-top: 25px;
+  
   display: flex;
   justify-content: space-between;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
+  border: 1px solid black
+  border-radius: 10px;
+  border-radius: 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-  list-style-type: none;
+  
 `;
 
 export const ListItem = styled.li`
   display: flex;
-  flex-direction: column;
+  gap:10px;
   align-items: center;
   justify-content: center;
   border-right: 1px solid gray;
   flex: 1;
   text-align: center;
-
-  &:not(:last-child) {
-    border-right: 1px solid gray;
-  }
 `;
 
 export const Label = styled.span`
@@ -85,4 +87,5 @@ export const Quantity = styled.span`
   justify-content: center;
   font-weight: 700;
   font-size: 18px;
+  color: black
 `;
